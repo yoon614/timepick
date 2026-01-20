996355c5cd44907ddf13b11f2c4880bca0bf7742
heejin <eein0522@naver.com>
2026-01-21 05:09:39 +0900

    💄Design : 타임픽 및 공고 화면구성

---
+++ app/src/main/java/com/example/timepick/JobEmptyActivity.kt
@@ -0,0 +1,20 @@
+package com.example.timepick
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class JobEmptyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_job_empty)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
